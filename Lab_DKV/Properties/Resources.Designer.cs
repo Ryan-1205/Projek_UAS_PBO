c2510764fb3fd918b68643cc99badc29bd66ff22
Ryan-1205 <ryanardiansyah735@gmail.com>
2025-12-16 21:49:42 +0700

    Alhamdulillah Selesai Final

--- Lab_DKV/Properties/Resources.Designer.cs
+++ Lab_DKV/Properties/Resources.Designer.cs
@@ -171,6 +171,16 @@ namespace Lab_DKV.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Icon_Berangkat {
+            get {
+                object obj = ResourceManager.GetObject("Icon_Berangkat", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap notview {
